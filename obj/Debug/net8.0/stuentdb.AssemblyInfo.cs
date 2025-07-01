ae3d5effc503ead4ce9735fc2457b48f1d84b2aa
Omkar-23 <omkarvya63@gmail.com>
2025-07-02 00:51:16 +0530

    updated cshtml files

--- obj/Debug/net8.0/stuentdb.AssemblyInfo.cs
+++ obj/Debug/net8.0/stuentdb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stuentdb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b04c3f5c3c6b8d8ac77b63e5b607769957c478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f53ff50b892f88f26667dad3dd2e3ba41b2a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("stuentdb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stuentdb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
